41ca8fb1879c5c5d26dea6a2b8ee31520fbdd734
Ratting Gergely <ratting.gergo@uni-pannon.hu>
2021-08-03 09:34:52 +0200

    Remove offline view

--- resources/views/offline.blade.php
+++
@@ -1,6 +0,0 @@
-<x-app-layout>
-    <x-callout class="border-blue-300  prose-lg">
-        <p>Megszakadt az internet kapcsolatod. Ha újra helyreáll, tudod folytatni a szerkesztést.</p>
-    </x-callout>
-</x-app-layout>
-
