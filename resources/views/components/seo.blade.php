e07f841cc8f2cab93969916a5686260a14f5b7b7
Ratting Gergely <ratting.gergo@uni-pannon.hu>
2021-08-07 05:58:01 +0200

    fix typo

--- resources/views/components/seo.blade.php
+++ resources/views/components/seo.blade.php
@@ -3,6 +3,6 @@
 
     <meta name="description" content="{{ $description ?? "Csökkentsd az impulzus vásárlásaid, költekezz tudatosan, spórolj hatékonyan! Használd fél évig, és döntsd el utána, hogy megéri-e az árát." }}">
 
-    <meta property="og:title" content="{{ $title }} | Brosztom.hu">
+    <meta property="og:title" content="{{ $title }} | Beosztom.hu">
     <meta property="og:description" content="{{ $description ?? "Csökkentsd az impulzus vásárlásaid, költekezz tudatosan, spórolj hatékonyan! Használd fél évig, és döntsd el utána, hogy megéri-e az árát." }}">
 @endsection
