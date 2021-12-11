ad65a0424f7505c4d059d2f8ffc23e813b015dee
Ratting Gergely <ratting.gergo@uni-pannon.hu>
2021-12-11 23:38:30 +0100

    wip

--- resources/views/knowledge/oszd-be-a-penzed.blade.php
+++ resources/views/knowledge/oszd-be-a-penzed.blade.php
@@ -2,9 +2,18 @@
 
 <h2>3. Oszd be a pénzed</h2>
 
-<p>Összeszámoltad a pénzed és létrehoztad a perselyeket. Nincs más hátra, mint beosztani a pénzt. Ha tudtál célokat beállítani a perselyekhez, akkor nincs nehéz dolgod, mindössze el kell döntened, hogy melyek a fontosak, és először azokba tenni.</p>
+<p>Összeszámoltad a pénzed és létrehoztad a perselyeket. Nincs más hátra, mint szétosztani ezt a pénzt a perselyek között. Ez az egyik legizgalmasabb rész, nagy meglepetések érhetnek már itt is.</p>
 
-<p>Ha szeretnéd, meg is jelölheted a fontosakat, sőt, ha valamelyiknek határideje van, azt is beállíthatod (pl karácsonyra félre akarsz tenni 50.000-et ajándékra, vagy 4 hónap múlva jogsira kell 130.000 Ft). Ilyenkor segítségedre lehet a kis varázspálca a beosztható összeg mellett, ami segít beosztani a pénzed.</p>
+<x-figure name="tranzakcio" title="Pénz beosztásához kattints a perselyre, majd Beoszt gombra" />
+
+<p>Először azokba a perselyekbe tegyél pénzt, amik nélkülözhetetlenek: áram, víz, albérlet, étel. Első körben egy havi kiadásod összegét oszd be (pl áram 6.000, étel 60.000), ha a végén marad még pénzed, érdemes ezt 2-3 havira emelni, hogy ha elveszted a munkádat, akkor is legyen rájuk félretéve pénz.</p>
+
+<p>Ha ezzel megvagy, tegyél egy kis pénzt a Vésztartalék perselyedbe. Ezt a perselyt előbb utóbb fel fogod töltened 6 havi fizetéseddel. Hogy ennek mi értelme és miért fontos, <x-a href="{{ route('articles.show', 'vesztartalek-diohejban') }}">itt olvashatsz.</x-a></p>
+
+<p>Ezután a fennmaradó összeget oszd szét a maradék perselyeid között. <x-a href="{{ route('articles.show', 'hatekony-penzbeosztas') }}">A pénz helyes és hatékony beosztása elég összetett dolog</x-a>, indulásnál elég, ha ennyire figyelsz.</p>
+
+<p>Ha szeretnéd, meg is jelölheted a fontos perselyeket, sőt, ha valamelyiknek határideje van, azt is beállíthatod (pl karácsonyra félre akarsz tenni 50.000-et ajándékra, vagy 4 hónap múlva jogsira kell 130.000 Ft). Ilyenkor segítségedre lehet a kis varázspálca a beosztható összeg mellett, ami segít beosztani a pénzed.</p>
+
 
 <figure>
     <picture>
